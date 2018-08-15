21e166c78f263b39bb8df266d1d5593fd19db5c2
zellymiriam <nakiwuge@gmail.com>
2018-08-15 10:38:28 +0300

    add uri to connection

--- v1/models.py
+++ v1/models.py
@@ -10,11 +10,12 @@ class Database:
             
         else:
             self.conn = psycopg2.connect(
-                host=ec2-54-235-160-57.compute-1.amazonaws.com,
-                user=dejkpzjbrysvkp,
-                password=e44398dac8f32cdafd89972d4f254f311765bdb878c1dd327ce8f1576b692403,
-                dbname=df3terog2vbq3c
-                port=5432
+                host='ec2-54-235-160-57.compute-1.amazonaws.com',
+                user='dejkpzjbrysvkp',
+                password='e44398dac8f32cdafd89972d4f254f311765bdb878c1dd327ce8f1576b692403',
+                dbname='df3terog2vbq3c',
+                port='5432',
+                uri='postgres://dejkpzjbrysvkp:e44398dac8f32cdafd89972d4f254f311765bdb878c1dd327ce8f1576b692403@ec2-54-235-160-57.compute-1.amazonaws.com:5432/df3terog2vbq3c'
         )
         self.conn.autocommit = True
         self.c = self.conn.cursor()
