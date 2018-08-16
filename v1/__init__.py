9b5b9c9d4ced3b270e1d1fbe06c4c84efd8d715e
zellymiriam <nakiwuge@gmail.com>
2018-08-16 15:45:37 +0300

    add cors

--- v1/__init__.py
+++ v1/__init__.py
@@ -1,15 +1,14 @@
 import os
 from flask import Flask
 from flask_jwt_extended import  JWTManager
-
-
+from flask_cors import CORS
 
 '''creating the app'''
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY']= 'secret'
 jwt = JWTManager(app)
-
+CORS(app)
 
 ''' importing routes '''
 from v1 import views
