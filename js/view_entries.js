e1c4ef9419d97b18c133f98328d1f43dd8a55d14
zellymiriam <nakiwuge@gmail.com>
2018-08-18 16:49:13 +0300

    store id

--- js/view_entries.js
+++ js/view_entries.js
@@ -23,12 +23,13 @@ function viewEntries(){
                 let entries=data["entries"]
                 if (entries){
                     entries.forEach(entry => {
+                        let get_id = localStorage.setItem("id",entry.entry_id)
                         document.getElementById("view_id").innerHTML += `   
                             <tr>
-                                <td ><a href="#">${entry.title}</a></td>
+                                <td ><a onClick=${get_id} href="view_content.html">${entry.title}</a></td>
                                 <td >${entry.date}</td>
                                 <td>
-                                    <form action="#">
+                                    <form action="view_content.html">
                                         <button class ="button-small"type="submit" >view</button>
                                     </form>
                                 </td>
@@ -41,6 +42,43 @@ function viewEntries(){
                     `
                         
                     })
+                   
+                }
+                else{
+                    document.getElementById('no_entry').innerHTML = `
+                    You have no entries. Please click Add entry to add an entry.
+                    `
+                }
+            }
+        })
+    }
+}
+
+function viewContent(){
+    let token = localStorage.getItem("token")
+  
+    if (token){
+        fetch(url,{
+            method: "GET",
+            headers: {
+                "content-type": "application/json",
+                "Authorization": "Bearer "+token
+            }
+
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data["msg"]=="Token has expired"){
+                window.location = "./index.html"
+            }
+            else{
+                console.log(data)
+                let entries=data["entries"]
+                if (entries){
+                    
+                    document.getElementById("view_title").innerHTML = entries
+                        
+                   
                    
                 }
                 else{
