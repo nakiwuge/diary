8b0b36bb0dd962dc1da02d3b06514b33e8b46a46
zellymiriam <nakiwuge@gmail.com>
2018-08-26 16:17:50 +0300

    add config for heroku

--- config.py
+++ config.py
@@ -1,7 +1,23 @@
 import os
 import psycopg2
 
-
+class Develop:
+    DEBUG = True
+    conn = psycopg2.connect(
+        host=os.environ.get('HOST'),
+        user=os.environ.get('DBUSER'),
+        password=os.environ.get('PASSWORD'),
+        dbname=os.environ.get('DATABASE')
+        )
+    
+class Testing:
+    DEBUG = True
+    conn = psycopg2.connect(
+        host=os.environ.get('HOST'),
+        user=os.environ.get('DBUSER'),
+        password=os.environ.get('PASSWORD'),
+        dbname=os.environ.get('TESTDB')
+        )
 
 class Production:
     DEBUG = False
--- v1/__init__.py
+++ v1/__init__.py
@@ -2,12 +2,14 @@ from os import environ
 from flask import Flask
 from flask_jwt_extended import  JWTManager
 from flask_cors import CORS
-from config import app_config
+from config import app_config, Production
 '''creating the app'''
 
 env = environ.get("APP_SETTINGS")
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY']= 'secret'
+if env == "production":
+    app.config.from_object(Production)
 
 app.config.from_object(app_config[env])
 print(app_config[env])
