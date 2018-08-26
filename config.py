2cb5e55145b338629ea4e73bef16b4404bf3815c
zellymiriam <nakiwuge@gmail.com>
2018-08-26 15:02:56 +0300

    add config for heroku

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
 import os
 import psycopg2
 
-class Production:
-    DEBUG = False
-    conn = psycopg2.connect(
-        host='ec2-54-235-160-57.compute-1.amazonaws.com',
-        user='dejkpzjbrysvkp',
-        password='e44398dac8f32cdafd89972d4f254f311765bdb878c1dd327ce8f1576b692403',
-        dbname='df3terog2vbq3c',
-        port='5432'
-        )
-
 class Develop:
     DEBUG = True
     conn = psycopg2.connect(
@@ -29,7 +19,15 @@ class Testing:
         dbname=os.environ.get('TESTDB')
         )
 
-
+class Production:
+    DEBUG = False
+    conn = psycopg2.connect(
+        host='ec2-54-235-160-57.compute-1.amazonaws.com',
+        user='dejkpzjbrysvkp',
+        password='e44398dac8f32cdafd89972d4f254f311765bdb878c1dd327ce8f1576b692403',
+        dbname='df3terog2vbq3c',
+        port='5432'
+        )
 
 app_config = {
      "develop": Develop,
