68d5a8ec5abe1366549d0063bdce9facf6ce7a36
zellymiriam <nakiwuge@gmail.com>
2018-08-26 15:48:18 +0300

    add config for heroku

--- v1/__init__.py
+++ v1/__init__.py
@@ -5,11 +5,9 @@ from flask_cors import CORS
 from config import app_config
 '''creating the app'''
 
-env = environ['APP_SETTINGS']
+env = environ.get("APP_SETTINGS")
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY']= 'secret'
-if env == "production":
-    app.config.from_object(app_config["production"])
 
 app.config.from_object(app_config[env])
 print(app_config[env])
