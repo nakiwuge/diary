0759930c3fe4ca43d1f6f2278a3ea75ef4839c44
zellymiriam <nakiwuge@gmail.com>
2018-08-26 15:34:26 +0300

    add config for heroku

--- v1/__init__.py
+++ v1/__init__.py
@@ -8,6 +8,9 @@ from config import app_config
 env = environ['APP_SETTINGS']
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY']= 'secret'
+if env == "production":
+    app.config.from_object(app_config["production"])
+
 app.config.from_object(app_config[env])
 print(app_config[env])
 jwt = JWTManager(app)
