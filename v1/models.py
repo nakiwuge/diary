a979630cd4d14ce62e8d94012625793d0bcd131a
zellymiriam <nakiwuge@gmail.com>
2018-08-26 18:47:13 +0300

    move db connection to models.py

--- v1/models.py
+++ v1/models.py
@@ -14,11 +14,11 @@ class Database:
                 )
         elif env == "production":
             self.conn = psycopg2.connect(
-                host=os.environ.get('HOST'),
-                user=os.environ.get('USER'),
-                password=os.environ.get('PASSWORD'),
-                dbname=os.environ.get('HEROKUDB'),
-                port=os.environ.get('PORT')
+                host='ec2-54-235-160-57.compute-1.amazonaws.com',
+                user='dejkpzjbrysvkp',
+                password='e44398dac8f32cdafd89972d4f254f311765bdb878c1dd327ce8f1576b692403',
+                dbname="df3terog2vbq3c",
+                port='5432'
                 )
         else:
             self.conn = psycopg2.connect(
